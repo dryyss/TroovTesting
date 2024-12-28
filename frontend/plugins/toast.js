9ffaf2eb19ac3b94b2568cd7502d5c6156fc72cd
dryyss <andrys.magar@hotmail.fr>
2024-12-28 23:05:51 +0100

    feat: finalisation du design de base

--- frontend/plugins/toast.js
+++ frontend/plugins/toast.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
-Vue.use(Toast, {
-  transition: 'Vue-Toastification__bounce',
-  maxToasts: 3,
-  newestOnTop: true,
-})
+// Vérifier si nous sommes côté client
+if (process.client) {
+  Vue.use(Toast, {
+    transition: 'Vue-Toastification__bounce',
+    maxToasts: 3,
+    newestOnTop: true,
+  })
+}
